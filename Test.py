683e64d5b22ad7f3deef04b570079fb83bc76487
Puja <pujamohata96@gmail.com>
2020-08-28 20:16:34 +0530

    added edit part

--- Test.py
+++ Test.py
@@ -18,6 +18,8 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor() # Defining global mycursor var here
 b = ''
+c = 0
+loop = 'y'
 def MontoTxt(month): # Function to convert month from digit to word
 	if(month == '01'):
 		b = 'January'
@@ -48,6 +50,9 @@ def MontoTxt(month): # Function to convert month from digit to word
 
 save = 0 # Savings var
 datee = ""
+val= ""
+h= ""
+cat = ""
 def getdate(): # Function that will enter date as and when required
 	global datee
 	datee = input("Enter date in the format YYYY-MM-DD : ")
@@ -69,187 +74,253 @@ def getdate(): # Function that will enter date as and when required
 		print("Sorry I can't travel to future")
 		getdate()
 
-print("Enter 1 to add details \n")
-print("Enter 2 to view details \n")
-print("Enter 3 to edit details \n")
-
-user = int(input())
-
-#Entering details as user pressed 1 to add details option 
-if (user == 1):
-	getdate()
-	income = 0 # define income as 0 for later use in calc last day expenditure
-	if (datee[8:10] == '01'): # Take income only if it's the first day of the month 
-		income = int(input("Enter your monthly income: "))
-	rent = int(input("Add your monthly rent expense : ") or 0) 
-	grocery = int(input("Add your monthly grocery expense : ") or 0)
-	travel = int(input("Add your travel expense for the day : ") or 0)
-	internet = int(input("Add your monthly internet charges : ") or 0)
-	dailyitem = int(input("Add your daily item expenses : ") or 0)
-	misc = int(input("Add your daily miscellaneous expenses : ") or 0)
-	#income = int(input("Enter your monthly income: "))
-	total = rent + grocery + travel + internet + dailyitem + misc
-	print("Your Total Expense for the day is : ",total) # Printing Total expense every day after entering data
+def validatedate():
+	global val
+	global h
+	global c
+	if c == 0:
+		val = input("Enter the Initial date(YYYY-MM-DD) : ")
 	
-	#weekly expenditure of the current week starting from Monday and ending on Sunday 
-	if (datetime.today().isoweekday() == 7):
-		calcweek(date1)
-		today = date.today()
-		date1 = today.strftime("%Y-%m-%d")
-		a = "%v"
-		sql = "SELECT SUM(Rent)+ SUM(Grocery) + SUM(Travel)+SUM(Internet)+SUM(DailyItem)+SUM(Miscellaneous) FROM `expenses` WHERE DATE_FORMAT(`Date`, '%s') = DATE_FORMAT('%s', '%s')" % (a,date1,a)
-		mycursor.execute(sql, date1)
-		result = mycursor.fetchone()
-		result1 = result[0] + rent + grocery + travel + internet + dailyitem + misc
-		print("Your this Week's Expenditure is %s" % result1)
-
-	#2020-08-31 
-	# 31 == lastdateof 2020 08
+	try:
+		#val = input("Enter the Initial date(YYYY-MM-DD) : ")
+		datetime.strptime(val,"%Y-%m-%d")
+		c = 1
+		
+	except ValueError:
+		print("This is the incorrect initial date string format. It should be YYYY-MM-DD")
+		c = 0
+		validatedate()
 	
-	if (int(datee[8:10]) == int(calendar.monthrange(int(datee[:4]),int(datee[6:7]))[1])):
-		#2020-08-01
-		#'%-08-%'
-		value ="%-"+ datee[5:7] +"-%"
-		sql = "SELECT SUM(Rent) + SUM(Grocery) + SUM(Travel) + SUM(Internet) + SUM(DailyItem) + SUM(Miscellaneous) FROM `expenses` WHERE `Date` LIKE '%s'" % value
-		mycursor.execute(sql, value)
-		res = mycursor.fetchone()
-		#Printing Monthly expenditure for the current month
-		print("Your Monthly Expenditure for %s is %s" %(MontoTxt(datee[5:7]),res[0]))
-		val = "%-"+datee[5:7] +"-01"
-		sql = "SELECT `Income` FROM expenses WHERE `Date` LIKE '%s'" % val
-		#val = (datee[4:10])
-		mycursor.execute(sql, val)
-		income1 = mycursor.fetchone()
-		print(type(income1[0]))
-		if(int(income1[0]) != 0):
-			save = income1[0] - res[0]
-			print(save)
-			print("Your Monthly Savings for %s is %s" %(MontoTxt(datee[5:7]),save))
-			
-		else:
-			print("You didn't Enter your income for the month")
-			income = int(input("Enter Your Income for the month : "))
-			save = income - res[0]
-			print("Your Monthly Savings for %s is %s" %(MontoTxt(datee[5:7]),save))
+	if c == 1:
+		h = input("Enter the Final date(YYYY-MM-DD): ")	
+	try:
+		#h = input("Enter the Final date(YYYY-MM-DD): ")	
+		datetime.strptime(h,"%Y-%m-%d")
+		
+	except ValueError:
+		print("This is the incorrect final date string format. It should be YYYY-MM-DD")
+		h = ""
+		c = 1
+		validatedate()
 
-	#Inserting Values after working through the above lines
-	sql = "INSERT INTO expenses (Date,Rent,Grocery,Travel,Internet,DailyItem,Miscellaneous,Income,Saving) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-	val = (datee, rent, grocery, travel, internet, dailyitem, misc, income,save)
-	mycursor.execute(sql, val)
+def GetCategory():
+	global cat
+	List = ['Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous']
+	print("Select the category('Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous') in which you wanna edit ")
+	cat = input()
+	cat = cat.capitalize()
+	if a not in List :
+		GetCategory()
+		
 
-	mydb.commit()
+while loop == 'y' :
+		
 
-	print(mycursor.rowcount, "record inserted.") # print for fun 
-   
-if (user == 2):
-	#getdate()
-	print("Enter 1 for viewing for a day")
-	print("Enter 2 for viewing for a Range of dates")
-	print("Enter 3 for viewing for a Range of months")
-	a = int(input())
-	if (a == 1):
+	print("Enter 1 to add details \n")
+	print("Enter 2 to view details \n")
+	print("Enter 3 to edit details \n")
+
+	user = int(input())
+
+	#Entering details as user pressed 1 to add details option 
+	if (user == 1):
 		getdate()
-		sql = "SELECT * FROM expenses WHERE `Date` = '%s'" %(datee)
-		mycursor.execute(sql)
-		myresult = mycursor.fetchall()
-		#print (myresult)
+		income = 0 # define income as 0 for later use in calc last day expenditure
+		if (datee[8:10] == '01'): # Take income only if it's the first day of the month 
+			income = int(input("Enter your monthly income: "))
+		rent = int(input("Add your monthly rent expense : ") or 0) 
+		grocery = int(input("Add your monthly grocery expense : ") or 0)
+		travel = int(input("Add your travel expense for the day : ") or 0)
+		internet = int(input("Add your monthly internet charges : ") or 0)
+		dailyitem = int(input("Add your daily item expenses : ") or 0)
+		misc = int(input("Add your daily miscellaneous expenses : ") or 0)
+		#income = int(input("Enter your monthly income: "))
+		total = rent + grocery + travel + internet + dailyitem + misc
+		print("Your Total Expense for the day is : ",total) # Printing Total expense every day after entering data
 		
-		df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
-		print(df)
+		#weekly expenditure of the current week starting from Monday and ending on Sunday 
+		if (datetime.today().isoweekday() == 7):
+			calcweek(date1)
+			today = date.today()
+			date1 = today.strftime("%Y-%m-%d")
+			a = "%v"
+			sql = "SELECT SUM(Rent)+ SUM(Grocery) + SUM(Travel)+SUM(Internet)+SUM(DailyItem)+SUM(Miscellaneous) FROM `expenses` WHERE DATE_FORMAT(`Date`, '%s') = DATE_FORMAT('%s', '%s')" % (a,date1,a)
+			mycursor.execute(sql, date1)
+			result = mycursor.fetchone()
+			result1 = result[0] + rent + grocery + travel + internet + dailyitem + misc
+			print("Your this Week's Expenditure is %s" % result1)
+
+		#2020-08-31 
+		# 31 == lastdateof 2020 08
 		
-	
-	
-	elif  a == 2:
-		print("Enter the Initial date(YYYY-MM-DD) : ")
-		val = input()
-		print("Enter the Final date(YYYY-MM-DD): ")
-		h = input()
-		#val = "-" + datee[5:7]+ "-" + datee[8:10]# %-08-% 
-		#h = "-" + datee[5:7]+ "-" + datee[8:10]
-		sql = "SELECT * FROM expenses WHERE `Date` BETWEEN '%s' AND '%s'" % (val,h)
-		mycursor.execute(sql)
-		myresult = mycursor.fetchall()
+		if (int(datee[8:10]) == int(calendar.monthrange(int(datee[:4]),int(datee[6:7]))[1])):
+			#2020-08-01
+			#'%-08-%'
+			value ="%-"+ datee[5:7] +"-%"
+			sql = "SELECT SUM(Rent) + SUM(Grocery) + SUM(Travel) + SUM(Internet) + SUM(DailyItem) + SUM(Miscellaneous) FROM `expenses` WHERE `Date` LIKE '%s'" % value
+			mycursor.execute(sql, value)
+			res = mycursor.fetchone()
+			#Printing Monthly expenditure for the current month
+			print("Your Monthly Expenditure for %s is %s" %(MontoTxt(datee[5:7]),res[0]))
+			val = "%-"+datee[5:7] +"-01"
+			sql = "SELECT `Income` FROM expenses WHERE `Date` LIKE '%s'" % val
+			#val = (datee[4:10])
+			mycursor.execute(sql, val)
+			income1 = mycursor.fetchone()
+			print(type(income1[0]))
+			if(int(income1[0]) != 0):
+				save = income1[0] - res[0]
+				print(save)
+				print("Your Monthly Savings for %s is %s" %(MontoTxt(datee[5:7]),save))
+				
+			else:
+				print("You didn't Enter your income for the month")
+				income = int(input("Enter Your Income for the month : "))
+				save = income - res[0]
+				print("Your Monthly Savings for %s is %s" %(MontoTxt(datee[5:7]),save))
+
+		#Inserting Values after working through the above lines
+		sql = "INSERT INTO expenses (Date,Rent,Grocery,Travel,Internet,DailyItem,Miscellaneous,Income,Saving) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+		val = (datee, rent, grocery, travel, internet, dailyitem, misc, income,save)
+		mycursor.execute(sql, val)
+
+		mydb.commit()
+
+		print(mycursor.rowcount, "record inserted.") # print for fun 
+	   
+	if (user == 2):
+		#getdate()
+		print("Enter 1 for viewing for a day")
+		print("Enter 2 for viewing for a Range of dates")
+		print("Enter 3 for viewing for a Range of months")
+		a = int(input())
+		if (a == 1):
+			getdate()
+			sql = "SELECT * FROM expenses WHERE `Date` = '%s'" %(datee)
+			mycursor.execute(sql)
+			myresult = mycursor.fetchall()
+			#print (myresult)
+			
+			df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
+			print(df)
+			
 		
-		df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
-		print(df)
 		
-	
-	elif a == 3:
-		print("select a for Current month")
-		print("select b for Last month")
-		print("select c for Last 3 months")
-		v = input()
-		if (v == 'a'):
-			r = date.today()
-			t = r.strftime("%Y-%m")
-			t = t + "-%"
-			sql = "SELECT * FROM expenses WHERE `Date` LIKE '%s'" %(t)
+		elif  a == 2:
+			validatedate()
+			sql = "SELECT * FROM expenses WHERE `Date` BETWEEN '%s' AND '%s'" % (val,h)
+			mycursor.execute(sql)
+			myresult = mycursor.fetchall()
+			
+			df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
+			print(df)
+			
+		
+		elif a == 3:
+			print("select a for Current month")
+			print("select b for Last month")
+			print("select c for Last 3 months")
+			v = input()
+			if (v == 'a'):
+				r = date.today()
+				t = r.strftime("%Y-%m")
+				t = t + "-%"
+				sql = "SELECT * FROM expenses WHERE `Date` LIKE '%s'" %(t)
+				#print(sql)
+				mycursor.execute(sql)
+				myresult = mycursor.fetchall()
+				#print(myresult)
+				df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
+				print(df)
+				
+			if (v == 'b'):
+				today = date.today()
+				if today.month == 1:
+					one_month_ago = today.replace(year=today.year - 1, month=12)
+		
+				else:
+					one_month_ago = today.replace(month=today.month - 1)
+		
+				z = one_month_ago.strftime("%Y-%m")
+				z= z + "-%" 
+				sql = "SELECT * FROM expenses WHERE `Date` LIKE '%s'" %(z)
+				mycursor.execute(sql)
+				myresult = mycursor.fetchall()
+				
+				df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
+				print(df)
+				
+			if (v == 'c'):
+				today = date.today()
+				#date_str = '2020-01'
+				#today = datetime.strptime(date_str, '%Y-%m').date()
+				#today = "2020-02-01"
+				#today = time.strptime(today, "%Y-%m")
+				y = today.strftime("%Y-%m")
+				y += "-31"
+				if today.month == 2:
+					two_month_ago = today.replace(year=today.year - 1, month=12)
+					
+				elif today.month == 1:
+					two_month_ago = today.replace(year=today.year - 1, month=11)
+					
+				else:
+					two_month_ago = today.replace(month=today.month - 2)
+				
+				z = two_month_ago.strftime("%Y-%m")
+				z = z+ "-01"
+				sql = "SELECT * FROM expenses WHERE `Date` BETWEEN '%s' AND '%s'" %(z,y)
+				print(sql)
+				mycursor.execute(sql)
+				myresult = mycursor.fetchall()
+				
+				df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
+				print(df)
+				
+		# else:
+			# getdate()
+			# val = "%-"+datee[5:7]+ "-%" # %-08-%
+			# sql = "SELECT SUM(%s) FROM expenses WHERE `Date` LIKE '%s'" %(a,val)
+			# val = (datee[4:10])
+			# mycursor.execute(sql, val)
+			# value = mycursor.fetchone()
+			# print(value[0])
+			
+	if (user == 3):
+		print("select 1 to edit one category for a day")
+		print("select 2 to edit all categories for a day")
+		a = int(input())
+		if (a == 1):
+			GetCategory()	
+			getdate()
+			b = input("Enter the new value : ")     
+			sql = "UPDATE expenses SET `%s` = %s WHERE Date = '%s'  " %(a,b,datee)
 			#print(sql)
+			#sql = "UPDATE expenses SET Rent = %s WHERE Date = %s"
+			#val = (b,datee)
 			mycursor.execute(sql)
-			myresult = mycursor.fetchall()
-			#print(myresult)
-			df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
-			print(df)
+
+			mydb.commit()
+
+			print(mycursor.rowcount, "record(s) affected")
+							
+		if (a == 2):
+			getdate()
+			grocery = int(input("Add your monthly grocery expense : ") or 0)
+			travel = int(input("Add your travel expense for the day : ") or 0)
+			internet = int(input("Add your monthly internet charges : ") or 0)
+			dailyitem = int(input("Add your daily item expenses : ") or 0)
+			misc = int(input("Add your daily miscellaneous expenses : ") or 0)
 			
-		if (v == 'b'):
-			today = date.today()
-			if today.month == 1:
-				one_month_ago = today.replace(year=today.year - 1, month=12)
-    
-			else:
-				one_month_ago = today.replace(month=today.month - 1)
-    
-			z = one_month_ago.strftime("%Y-%m")
-			z= z + "-%" 
-			sql = "SELECT * FROM expenses WHERE `Date` LIKE '%s'" %(z)
+			sql = "UPDATE expenses SET Grocery = %s, Travel = %s, Internet = %s, DailyItem = %s, Miscellaneous = %s WHERE Date = '%s' " %(grocery,travel,internet,dailyitem,misc,datee) 
+			
 			mycursor.execute(sql)
-			myresult = mycursor.fetchall()
 			
-			df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
-			print(df)
+			mydb.commit()
 			
-		if (v == 'c'):
-			today = date.today()
-			#date_str = '2020-01'
-			#today = datetime.strptime(date_str, '%Y-%m').date()
-			#today = "2020-02-01"
-			#today = time.strptime(today, "%Y-%m")
-			y = today.strftime("%Y-%m")
-			y += "-31"
-			if today.month == 2:
-				two_month_ago = today.replace(year=today.year - 1, month=12)
-				
-			elif today.month == 1:
-				two_month_ago = today.replace(year=today.year - 1, month=11)
-				
-			else:
-				two_month_ago = today.replace(month=today.month - 2)
+			print(mycursor.rowcount, "record(s) affected")
+	print("Enter Y to continue")
+	loop = input()
+	loop = loop.lower()	
 			
-			z = two_month_ago.strftime("%Y-%m")
-			z = z+ "-01"
-			sql = "SELECT * FROM expenses WHERE `Date` BETWEEN '%s' AND '%s'" %(z,y)
-			print(sql)
-			mycursor.execute(sql)
-			myresult = mycursor.fetchall()
-			
-			df = pd.DataFrame(myresult,columns=['Date','Rent','Grocery','Travel','Internet','Daily Item','Miscellaneous','Income','Savings'])
-			print(df)
-			
-	else:
-		getdate()
-		val = "%-"+datee[5:7]+ "-%" # %-08-%
-		sql = "SELECT SUM(%s) FROM expenses WHERE `Date` LIKE '%s'" %(a,val)
-		#val = (datee[4:10])
-		mycursor.execute(sql, val)
-		value = mycursor.fetchone()
-		print(value[0])
 		
-if (user == 3):
-	print("Wanna Edit Your Expense of any day?")
-	getdate()
-	
-	
-	
-	
-		
-	
